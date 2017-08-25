3f8ef279631e35dc37930817077a1f5efd4ff90a
Jeff Arthur <wuftymerguftyguff@gmail.com>
2017-08-25 13:39:36 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import cmd
+import r53
+
+awsac="AKIAJKEIBTFAOMHZA3JQ"
+awssc="4NwB5cvRI+M5q41wUGdusQ7d0bGzMAkBdn3btRbF"
+
+
+
+class nsupdate53(cmd.Cmd):
+
+    UPDATECMDS = ["delete", "add"]
+
+    """Simple command processor example."""
+    def __init__(self):
+        self.r53 = r53.R53(awsac, awssc)
+        cmd.Cmd.__init__(self)
+
+
+    def do_update(self,args):
+        """Update a DNS Record"""
+        print "Doing Update"
+        subcmds=args.split()
+        updatecmd=subcmds[0]
+        updatehost=subcmds[1]
+        if updatecmd and updatecmd in self.UPDATECMDS:
+            print "Doing " + updatecmd
+            if updatehost and updatecmd == "add":
+              if 3 <= len(subcmds) < 4:
+                ip = subcmds[2]
+                self.r53.addArecord(updatehost,ip)
+            if updatehost and updatecmd == "delete":
+                if len(subcmds) == 2:
+                    self.r53.delArecord(updatehost)
+
+    def do_send(self,args):
+            self.r53.commit()
+
+    def emptyline(self):
+        pass
+
+    def do_gethostedzones(self,line):
+        """Get All Hosted Zones"""
+        print self.r53.getHostedZoneList()
+
+
+    def do_EOF(self, line):
+        return True
+
+
+if __name__ == '__main__':
+    u = nsupdate53()
+    u.prompt = '> '
+    u.cmdloop()
---
+++ r53.py
@@ -0,0 +1,93 @@
+from boto.route53.connection import Route53Connection
+from boto.route53.record import ResourceRecordSets
+import urllib2
+import re
+
+class R53:
+
+    changes = None
+
+    def __init__(self,awsac,awssc):
+        self.ar53 = Route53Connection(aws_access_key_id=awsac,aws_secret_access_key=awssc,debug=1)
+
+    def getExternalIP(self):
+        ext_ip = urllib2.urlopen('http://ipv4.icanhazip.com').read()
+        regex = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+        return unicode(regex.findall(ext_ip)[0])
+
+
+    def getHostedZoneList(self):
+        hostedZone = self.ar53.get_all_hosted_zones()
+        return hostedZone.ListHostedZonesResponse.HostedZones
+
+
+    def getResourceRecords(self,zoneID=None):
+        rr = self.ar53.get_all_rrsets(zoneID)
+        return rr
+
+
+    def getARecordForZone(self, zoneid, Zone):
+        Zone = self.validateZoneName(Zone)
+        rr = self.getResourceRecords(zoneid);
+        for rset in rr:
+            if rset.name == Zone and rset.type == "A":
+                return rset
+
+
+    def getARecord(self, zoneid, Zone, host):
+        Zone = self.validateZoneName(Zone)
+        rr = self.getResourceRecords(zoneid);
+        for rset in rr:
+            if rset.name == host + Zone and rset.type == "A":
+                return rset
+
+
+    def getZoneId(self, zone=None):
+        zone = self.validateZoneName(zone)
+        for Zone in self.getHostedZoneList():
+            if zone in Zone.Name:
+                ZoneId = Zone.Id.replace('/hostedzone/', '')
+                return ZoneId
+
+
+    def validateZoneName(self,zone=None):
+        if zone[-1] != ".":
+            zone = zone + "."
+        return zone
+
+    def zoneNameFromHostname(self,hostname=None):
+        if hostname:
+            s = '.'
+            splitted = hostname.split(s)[1:]
+            return s.join(splitted)
+
+    def addArecord(self,host,ip):
+        zone = self.zoneNameFromHostname(host)
+        zoneID = self.getZoneId(zone)
+        self._addRR(zoneID,"A",host,ip)
+
+    def delArecord(self, host):
+        zone = self.zoneNameFromHostname(host)
+        zoneID = self.getZoneId(zone)
+        self._delRR(zoneID, "A", host)
+
+    def _addRR(self,zoneid,rectype,host,ip,ttl=300):
+        if self.changes is None:
+            self.changes = ResourceRecordSets(self.ar53, zoneid)
+        change = self.changes.add_change("CREATE", host, rectype, ttl)
+        change.add_value(ip)
+
+
+    def _delRR(self,zoneid,rectype,host,ttl=300):
+        if self.changes is None:
+            self.changes = ResourceRecordSets(self.ar53, zoneid)
+        change = self.changes.add_change("DELETE", host, rectype, ttl)
+        change.add_value("4.3.2.1")
+
+    def commit(self):
+        if self.changes is not None:
+                self.changes.commit()
+                self.changes = None
+
+
+
